8c8c849bcf2c8651be5b43c72cbd748f8031d27c
kjqw <johnson0@keio.jp>
2024-09-12 03:34:21 +0900

    内容と満足度の両方を学習するようにした

--- functions.py
+++ functions.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import torch
+from classes import CustomDataset, TextClassifier
 from sklearn.preprocessing import LabelEncoder
+from torch.optim import AdamW
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 from transformers import AdamW, BertForSequenceClassification, BertTokenizer
@@ -71,3 +73,47 @@ def extract_long_text(input_path: Path, output_path: Path, min_length: int) -> N
 
     # TSVファイルとして保存
     long_texts.to_csv(output_path, sep="\t", index=False)
+
+
+# データローダー作成関数
+def create_dataloaders(
+    texts: pd.Series,
+    labels: pd.Series,
+    tokenizer: BertTokenizer,
+    batch_size: int,
+    max_len: int,
+) -> DataLoader:
+    dataset = CustomDataset(texts.tolist(), labels.tolist(), tokenizer, max_len)
+    return DataLoader(dataset, batch_size=batch_size)
+
+
+# 訓練関数
+def train_model(
+    classifier: TextClassifier,
+    train_texts: pd.Series,
+    val_texts: pd.Series,
+    train_labels: pd.Series,
+    val_labels: pd.Series,
+    max_len: int,
+    batch_size: int,
+    lr: float,
+    epochs: int,
+) -> None:
+    # オプティマイザーの初期化
+    optimizer = AdamW(classifier.model.parameters(), lr=lr)
+
+    # データローダー作成
+    train_loader = create_dataloaders(
+        train_texts, train_labels, classifier.tokenizer, batch_size, max_len
+    )
+    val_loader = create_dataloaders(
+        val_texts, val_labels, classifier.tokenizer, batch_size, max_len
+    )
+
+    # 訓練と評価
+    for epoch in range(epochs):
+        train_acc, train_loss = classifier.train(train_loader, optimizer, epoch)
+        val_acc = classifier.evaluate(val_loader)
+        print(
+            f"Epoch {epoch}: Train Loss: {train_loss}, Train Acc: {train_acc}, Val Acc: {val_acc}"
+        )
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 # %%
 from pathlib import Path
 
+import functions
 import pandas as pd
 import torch
 from classes import CustomDataset, TextClassifier
@@ -11,7 +12,7 @@ from torch.utils.data import DataLoader
 
 # %%
 BATCH_SIZE = 16
-EPOCHS = 10
+EPOCHS = 5
 MAX_LEN = 128  # 最大トークン数
 LR = 2e-5  # 学習率
 TEST_SIZE = 0.2  # テストデータの割合
@@ -21,7 +22,10 @@ MODEL_NAME_BERT = "google-bert/bert-base-multilingual-cased"
 # MODEL_NAME_ZEROSHOT = "MoritzLaurer/deberta-v3-large-zeroshot-v2.0"
 
 DATA_PATH = Path(__file__).parent / "data" / "data_long_texts_10.tsv"
-MODEL_SAVE_PATH = Path(__file__).parent / "saved_models" / "model_1"
+SAVE_PATH_CONTENT_MODEL = Path(__file__).parent / "saved_models" / "model_content_1"
+SAVE_PATH_SATISFACTION_MODEL = (
+    Path(__file__).parent / "saved_models" / "model_satisfaction_1"
+)
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -30,47 +34,98 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 df = pd.read_csv(DATA_PATH, delimiter="\t")
 
 # LabelEncoderを使用してラベルを数値にエンコード
-label_encoder = LabelEncoder()
-df["label_id"] = label_encoder.fit_transform(df["label"])
+label_encoder_content = LabelEncoder()
+label_encoder_satisfaction = LabelEncoder()
+df["label_id_content"] = label_encoder_content.fit_transform(df["label"])
+df["label_id_satisfaction"] = label_encoder_satisfaction.fit_transform(
+    df["satisfaction"]
+)
 
+# %%
 # 訓練データとテストデータの分割
-train_texts, val_texts, train_labels, val_labels = train_test_split(
-    df["text"], df["label_id"], test_size=TEST_SIZE, random_state=RANDOM_STATE
+(
+    train_texts,
+    val_texts,
+    train_labels_content,
+    val_labels_content,
+    train_labels_satisfaction,
+    val_labels_satisfaction,
+) = train_test_split(
+    df["text"],
+    df["label_id_content"],
+    df["label_id_satisfaction"],
+    test_size=TEST_SIZE,
+    random_state=RANDOM_STATE,
 )
 
 # %%
 # モデル初期化
 classifier_content = TextClassifier(
     MODEL_NAME_BERT,
-    num_labels=len(label_encoder.classes_),
+    num_labels=len(label_encoder_content.classes_),
     max_length=MAX_LEN,
     device=DEVICE,
 )
-optimizer = AdamW(classifier_content.model.parameters(), lr=LR)
 
 # %%
-# データセットの作成
-train_dataset = CustomDataset(
-    train_texts.tolist(), train_labels.tolist(), classifier_content.tokenizer, MAX_LEN
+# 訓練
+functions.train_model(
+    classifier_content,
+    train_texts,
+    val_texts,
+    train_labels_content,
+    val_labels_content,
+    MAX_LEN,
+    BATCH_SIZE,
+    LR,
+    EPOCHS,
 )
-val_dataset = CustomDataset(
-    val_texts.tolist(), val_labels.tolist(), classifier_content.tokenizer, MAX_LEN
-)
-
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE)
-
-# %%
-# 訓練と評価
-for epoch in range(EPOCHS):
-    train_acc, train_loss = classifier_content.train(train_loader, optimizer, epoch)
-    val_acc = classifier_content.evaluate(val_loader)
-    print(
-        f"Epoch {epoch}: Train Loss: {train_loss}, Train Acc: {train_acc}, Val Acc: {val_acc}"
-    )
-
 
 # %%
 # モデル保存
-classifier_content.save_model(MODEL_SAVE_PATH)
+classifier_content.save_model(SAVE_PATH_CONTENT_MODEL)
+
+# %%
+# 変数を削除
+del classifier_content
+
+# %%
+# GPUのメモリを解放
+torch.cuda.empty_cache()
+
+# %%
+# モデル初期化
+classifier_satisfaction = TextClassifier(
+    MODEL_NAME_BERT,
+    num_labels=len(label_encoder_satisfaction.classes_),
+    max_length=MAX_LEN,
+    device=DEVICE,
+)
+
+# %%
+# 訓練
+functions.train_model(
+    classifier_satisfaction,
+    train_texts,
+    val_texts,
+    train_labels_satisfaction,
+    val_labels_satisfaction,
+    MAX_LEN,
+    BATCH_SIZE,
+    LR,
+    EPOCHS,
+)
+
+# %%
+# モデル保存
+classifier_satisfaction.save_model(SAVE_PATH_SATISFACTION_MODEL)
+
+# %%
+# 変数を削除
+del classifier_satisfaction
+
+# %%
+# GPUのメモリを解放
+torch.cuda.empty_cache()
+
 # %%
