13eafc759bc71a648fa53e11488d2d582682a2b7
kjqw <johnson0@keio.jp>
2024-09-23 16:15:01 +0900

    推論後の評価の途中経過をコミット

--- evaluate.py
+++ evaluate.py
@@ -1,135 +1,119 @@
 # %%
+import pickle
 from pathlib import Path
 
-import functions
 import pandas as pd
-import torch
-from classes import CustomDataset, TextClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from torch.optim import AdamW
-from torch.utils.data import DataLoader
 
 # %%
-BATCH_SIZE = 16
-EPOCHS = 5
-MAX_LEN = 128  # 最大トークン数
-LR = 2e-5  # 学習率
-TEST_SIZE = 0.2  # テストデータの割合
-RANDOM_STATE = 0  # ランダムシード
-
-MODEL_NAME_BERT = "google-bert/bert-base-multilingual-cased"
-# MODEL_NAME_ZEROSHOT = "MoritzLaurer/deberta-v3-large-zeroshot-v2.0"
-
-DATA_PATH = Path(__file__).parent / "data" / "data_long_texts_10.tsv"
-SAVE_PATH_CONTENT_MODEL = Path(__file__).parent / "saved_models" / "model_content_1"
-SAVE_PATH_SATISFACTION_MODEL = (
-    Path(__file__).parent / "saved_models" / "model_satisfaction_1"
-)
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+MODEL_NUM = 10  # モデルの数
 
 # %%
-# データを読み込み、ラベルを数値に変換
-df = pd.read_csv(DATA_PATH, delimiter="\t")
-
-# LabelEncoderを使用してラベルを数値にエンコード
-label_encoder_content = LabelEncoder()
-label_encoder_satisfaction = LabelEncoder()
-df["label_id_content"] = label_encoder_content.fit_transform(df["label"])
-df["label_id_satisfaction"] = label_encoder_satisfaction.fit_transform(
-    df["satisfaction"]
-)
-
-# %%
-# 訓練データとテストデータの分割
-(
-    train_texts,
-    val_texts,
-    train_labels_content,
-    val_labels_content,
-    train_labels_satisfaction,
-    val_labels_satisfaction,
-) = train_test_split(
-    df["text"],
-    df["label_id_content"],
-    df["label_id_satisfaction"],
-    test_size=TEST_SIZE,
-    random_state=RANDOM_STATE,
-)
-# %%
-# モデルの読み込み
-classifier_content = TextClassifier(
-    MODEL_NAME_BERT,
-    num_labels=len(label_encoder_content.classes_),
-    max_length=MAX_LEN,
-    device=DEVICE,
-    load_model_path=SAVE_PATH_CONTENT_MODEL,
-)
-classifier_satisfaction = TextClassifier(
-    MODEL_NAME_BERT,
-    num_labels=len(label_encoder_satisfaction.classes_),
-    max_length=MAX_LEN,
-    device=DEVICE,
-    load_model_path=SAVE_PATH_SATISFACTION_MODEL,
-)
-
+# 結果を読み込み
+df_results_content = []
+df_results_satisfaction = []
+for i in range(MODEL_NUM):
+    with open(Path(__file__).parent / "data" / f"df_result_content_{i}.pkl", "rb") as f:
+        df_results_content.append(pickle.load(f))
+    with open(
+        Path(__file__).parent / "data" / f"df_result_satisfaction_{i}.pkl", "rb"
+    ) as f:
+        df_results_satisfaction.append(pickle.load(f))
 
 # %%
-# 予測関数の適用をまとめて行う
-def predict_and_compare(row, classifier, label_encoder):
-    prediction = classifier.predict_text(
-        row["text"], label_encoder
-    )  # textに対して予測を行う
-    correct = prediction == row["original_label"]  # 正解かどうかを判定
-    return pd.Series([prediction, correct], index=["predicted_label", "is_correct"])
+# 最初のデータフレームのtextとoriginal_labelを基準に他のデータフレームを結合
+df_result_content = df_results_content[0][["text", "original_label"]].copy()
 
-
-# %%
-# val_textsとval_labelsをDataFrameに変換
-df_content = pd.DataFrame(
-    {
-        "text": val_texts,
-        "original_label": label_encoder_content.inverse_transform(val_labels_content),
-    }
-)
-df_satisfaction = pd.DataFrame(
-    {
-        "text": val_texts,
-        "original_label": label_encoder_satisfaction.inverse_transform(
-            val_labels_satisfaction
+# 他のデータフレームからpredicted_labelとis_correctを追加していく
+for i in range(MODEL_NUM):
+    df_result_content = df_result_content.join(
+        df_results_content[i][["predicted_label", "is_correct"]].rename(
+            columns={
+                "predicted_label": f"predicted_label_{i+1}",
+                "is_correct": f"is_correct_{i+1}",
+            }
         ),
-    }
-)
-
-# %%
-# applyでclassifierとlabel_encoderを引数として渡すため、lambdaを使用
-
-# classifier_contentとlabel_encoder_contentを使って予測と正解判定
-df_result_content = df_content.join(
-    df_content.apply(
-        lambda row: predict_and_compare(row, classifier_content, label_encoder_content),
-        axis=1,
+        how="outer",
     )
+
+# is_correctのTrueとFalseをカウントする新しい列を作成
+df_result_content["true_count"] = df_result_content.filter(like="is_correct").apply(
+    lambda row: (row == True).sum(), axis=1
+)
+df_result_content["false_count"] = df_result_content.filter(like="is_correct").apply(
+    lambda row: (row == False).sum(), axis=1
 )
 
-# classifier_satisfactionとlabel_encoder_satisfactionを使って予測と正解判定
-df_result_satisfaction = df_satisfaction.join(
-    df_satisfaction.apply(
-        lambda row: predict_and_compare(
-            row, classifier_satisfaction, label_encoder_satisfaction
-        ),
-        axis=1,
-    )
-)
-# %%
-# 結果を表示
+# 結合された結果を表示
 df_result_content
+
+
 # %%
-df_result_satisfaction
+df_result_content["true_count"].sum(), df_result_content["false_count"].sum()
 # %%
-# 正解率を計算
-df_result_content["is_correct"].mean()
+# 全indexを取得
+indices_0 = df_results_content[0].index.tolist()
+indices_1 = df_results_content[1].index.tolist()
+
+common_indices = list(set(indices_0) & set(indices_1))
+common_indices
+
 # %%
-df_result_satisfaction["is_correct"].mean()
+# indexがnのデータを表示
+n = 1024
+print(df_results_content[0].loc[n])
+print(df_results_content[1].loc[n])
 # %%
+
+# # %%
+# # 正解率を集計
+# correct_rates_content_all = []
+# correct_rates_satisfaction_all = []
+# correct_rates_content_groupyby = []
+# correct_rates_satisfaction_groupby = []
+# for i in range(MODEL_NUM):
+#     correct_rates_content_all.append(df_results_content[i]["is_correct"].mean())
+#     correct_rates_content_groupyby.append(
+#         df_results_content[i].groupby("original_label")["is_correct"].mean()
+#     )
+#     correct_rates_satisfaction_all.append(
+#         df_results_satisfaction[i]["is_correct"].mean()
+#     )
+#     correct_rates_satisfaction_groupby.append(
+#         df_results_satisfaction[i].groupby("original_label")["is_correct"].mean()
+#     )
+
+# # %%
+# correct_rates_content_groupyby[0]
+# # %%
+# # 正解率の平均と分散を計算
+# correct_rates_content_all = pd.Series(correct_rates_content_all)
+# correct_rates_satisfaction_all = pd.Series(correct_rates_satisfaction_all)
+# correct_rates_content_groupyby = pd.DataFrame(correct_rates_content_groupyby).T
+# correct_rates_satisfaction_groupby = pd.DataFrame(correct_rates_satisfaction_groupby).T
+# correct_rates_content_all_mean = correct_rates_content_all.mean()
+# correct_rates_satisfaction_all_mean = correct_rates_satisfaction_all.mean()
+# correct_rates_content_all_std = correct_rates_content_all.std()
+# correct_rates_satisfaction_all_std = correct_rates_satisfaction_all.std()
+# correct_rates_content_groupyby_mean = correct_rates_content_groupyby.mean()
+# correct_rates_satisfaction_groupby_mean = correct_rates_satisfaction_groupby.mean()
+# correct_rates_content_groupyby_std = correct_rates_content_groupyby.std()
+# correct_rates_satisfaction_groupby_std = correct_rates_satisfaction_groupby.std()
+
+# # %%
+# # 結果を出力
+# print("Content Classification")
+# print(f"Mean: {correct_rates_content_all_mean:.2f}")
+# print(f"Std: {correct_rates_content_all_std:.2f}")
+# print()
+# print("Satisfaction Classification")
+# print(f"Mean: {correct_rates_satisfaction_all_mean:.2f}")
+# print(f"Std: {correct_rates_satisfaction_all_std:.2f}")
+# print()
+# print("Content Classification Groupby")
+# print(correct_rates_content_groupyby_mean)
+# print(correct_rates_content_groupyby_std)
+# print()
+# print("Satisfaction Classification Groupby")
+# print(correct_rates_satisfaction_groupby_mean)
+# print(correct_rates_satisfaction_groupby_std)
+# # %%
